62270996ab1520049735d05ff6c5bb7e48572b05
Sudhan M <98807138+sudh4n@users.noreply.github.com>
2024-10-08 22:37:08 +0530

    Basic Operations in Stack using Python.py

---
+++ Basic Operations in Stack using Python.py
@@ -0,0 +1,40 @@
+"""Push: Add an element to the top of a stack
+   Pop: Remove an element from the top of a stack
+   IsEmpty: Check if the stack is empty
+   IsFull: Check if the stack is full
+   Peek: Get the value of the top element without removing it"""
+
+# Stack implementation in python
+
+# Creating a stack
+def create_stack():
+    stack = []
+    return stack
+
+
+# Creating an empty stack
+def check_empty(stack):
+    return len(stack) == 0
+
+
+# Adding items into the stack
+def push(stack, item):
+    stack.append(item)
+    print("pushed item: " + item)
+
+
+# Removing an element from the stack
+def pop(stack):
+    if (check_empty(stack)):
+        return "stack is empty"
+
+    return stack.pop()
+
+
+stack = create_stack()
+push(stack, str(1))
+push(stack, str(2))
+push(stack, str(3))
+push(stack, str(4))
+print("popped item: " + pop(stack))
+print("stack after popping an element: " + str(stack))
